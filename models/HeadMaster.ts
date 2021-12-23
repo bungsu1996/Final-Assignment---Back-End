de9da19618c123d017be0f62aff9da805d19ce5f
nurjanahevs <evasitinurjanah2019@gmail.com>
2021-12-23 23:46:22 +0700

    update models

--- models/HeadMaster.ts
+++ models/HeadMaster.ts
@@ -2,21 +2,21 @@ import { model, Model, Schema } from "mongoose";
 import IHeadMaster from "../interface/headmaster.interface";
 
 const headMasterSchema = new Schema({
-  name: {type: String, required: true},
-  email: {type: String, unique: true, required: true},
-  password: {type: String, required: true},
-  adress: {type: String, required: true},
-  teachers: {type: String, required: true},
-  class: {type: String, required: true},
-  students: {type: String, required: true},
-  parent_students: {type: String, required: true},
-  schedule: {type: String, required: true},
-  study_lesson: {type: String, required: true},
-  grade: {type: Number, required: true},
-  score: {type: Number, required: true},
-  homeroom_teachers: {type: String, required: true},
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  adress: { type: String, required: true },
+  teachers: { type: String, required: true },
+  class: { type: String, required: true },
+  students: { type: Schema.Types.ObjectId, ref: "students", required: true },
+  parent_students: { type: Schema.Types.ObjectId, ref: "parent_students", required: true },
+  schedule: { type: Schema.Types.ObjectId, ref: "schedule", required: true },
+  study_lesson: { type: Schema.Types.ObjectId, ref: "study_lesson", required: true },
+  grade: { type: Schema.Types.ObjectId, ref: "grade", required: true },
+  score: { type: Schema.Types.ObjectId, ref: "score", required: true },
+  homeroom_teachers: { type: Schema.Types.ObjectId, ref: "homeroom_teachers", required: true },
 });
 
 const HeadMaster: Model<IHeadMaster> = model<IHeadMaster>("HeadMaster", headMasterSchema);
 
-export default HeadMaster; 
+export default HeadMaster;
