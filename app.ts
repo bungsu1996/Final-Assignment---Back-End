b4600d804b5e57aa39df5ca44d1a357946990102
bungsu1996 <venomous.zach@gmail.com>
2021-12-29 02:55:54 +0700

    new update error handler

---
+++ app.ts
@@ -0,0 +1,49 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import mongoDB from "./configs/db";
+import routes from "./routes/routes";
+import { errorHandler } from "./middlewares/errorHandler";
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.plugin();
+    this.route();
+    this.errorHandler();
+  }
+
+  protected plugin = () => {
+    mongoDB.connect();
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(function (req: Request, res: Response, next: NextFunction) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+      );
+      next();
+    });
+  };
+
+  protected route = () => {
+    this.app.use("/api", routes);
+  };
+  protected errorHandler = () => {
+    this.app.use(errorHandler.errHandle);
+  };
+}
+
+const app = new App().app;
+const port = process.env.PORT || 3535;
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
