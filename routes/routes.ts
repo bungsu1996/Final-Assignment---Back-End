56a9fc8fe4c3dab4b4aa2931c612a99812855269
bungsu1996 <venomous.zach@gmail.com>
2021-12-29 01:05:02 +0700

    new update teacher controller

--- routes/routes.ts
+++ routes/routes.ts
@@ -3,26 +3,27 @@ import users from "../controllers/user.controller";
 import courseRoutes from "./course.routes";
 import scheduleRoutes from "./schedule.routes";
 import schoolworkRoutes from "./schoolwork.routes";
+import { teacherRouter } from "./teacher.routes";
 
 class Routes {
-    router: Router;
+    public router: Router;
     constructor() {
         this.router = Router();
         this.routes();
+        this.teacherRoute();
     }
-
     routes = () => {
         this.router.get("/", (req: Request, res: Response) => {
             res.status(200).json({ message: "Success!" });
         });
-
         this.router.post("/register", users.register);
-
         this.router.post("/login", users.login);
-
         this.router.use("/schoolwork", schoolworkRoutes);
         this.router.use("/course", courseRoutes);
         this.router.use("/schedule", scheduleRoutes);
+    };
+    protected teacherRoute = () => {
+        this.router.use("/teacher", teacherRouter);
     };
 }
 
