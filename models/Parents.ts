6b5387ab44ceeb98218b73fd44db4960639d69bf
Diar J R <diar.student@gmail.com>
2022-01-09 19:17:13 +0700

    update routes, controller, model v1

--- models/Parents.ts
+++ models/Parents.ts
@@ -2,13 +2,8 @@ import { model, Model, Schema } from "mongoose";
 import IParents from "../interfaces/IParents";
 
 const parentSchema = new Schema({
-<<<<<<< HEAD
-    email: { type: String, required: true },
-    password: { type: String, required: true, default: "1234abcd" },
-=======
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, default: '1234abcd' },
->>>>>>> 2f95f8ee60ebdb44d437d07ec50a8b9cb848667a
+    password: { type: String, required: true, default: "1234abcd" },
     fullName: { type: String, required: true },
     birthDate: { type: Date, required: true },
     student: { type: Schema.Types.ObjectId, ref: "student" },
