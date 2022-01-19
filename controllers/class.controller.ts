286673ebcd6d12d2e8046766d86eaad3a1349723
Diar J R <diar.student@gmail.com>
2022-01-19 20:48:46 +0700

    update routing

--- controllers/class.controller.ts
+++ controllers/class.controller.ts
@@ -5,188 +5,185 @@ import Student from "../models/Students";
 import Teacher from "../models/Teachers";
 
 class ClassConttroller {
-  static async createClass(req: Request, res: Response, next: NextFunction) {
-    const { className, yearAcademic, semester } = req.body;
-    try {
-      const result = await Class.create({
-        className: className,
-        yearAcademic: yearAcademic,
-        semester: semester,
-      });
-      res.status(201).json(result);
-    } catch (error) {
-      next(error);
+    static async createClass(req: Request, res: Response, next: NextFunction) {
+        const { className, yearAcademic, semester } = req.body;
+        try {
+            const result = await Class.create({
+                className: className,
+                yearAcademic: yearAcademic,
+                semester: semester,
+            });
+            res.status(201).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async setHomeroom(req: Request, res: Response, next: NextFunction) {
-    const { classes, homeroom } = req.body;
-    try {
-      const foundTeach = await Teacher.findById({ _id: homeroom });
-      if (!foundTeach) {
-        throw { name: "NOT_FOUND_TEACHER" };
-      }
-      const foundClasses = await Class.findById({ _id: classes });
-      if (!foundClasses) {
-        throw { name: "NOT_FOUND_CLASS" };
-      }
-      const result = await Class.findByIdAndUpdate(
-        foundClasses,
-        {
-          homeTeacher: foundTeach,
-        },
-        { new: true }
-      );
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
+    static async setHomeroom(req: Request, res: Response, next: NextFunction) {
+        const { classes, homeroom } = req.body;
+        try {
+            const foundTeach = await Teacher.findById({ _id: homeroom });
+            if (!foundTeach) {
+                throw { name: "NOT_FOUND_TEACHER" };
+            }
+            const foundClasses = await Class.findById({ _id: classes });
+            if (!foundClasses) {
+                throw { name: "NOT_FOUND_CLASS" };
+            }
+            const result = await Class.findByIdAndUpdate(
+                foundClasses,
+                {
+                    homeTeacher: foundTeach,
+                },
+                { new: true }
+            );
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async scorebyClass(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
-    try {
-      const foundClass = await Class.findById(id);
-      if (!foundClass) {
-        throw { name: "NOT_FOUND_CLASS" };
-      }
-      const result = await Class.findById(foundClass)
-        .select("student")
-        .populate({ path: "student", select: "fullName score" });
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
+    static async scorebyClass(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        try {
+            const foundClass = await Class.findById(id);
+            if (!foundClass) {
+                throw { name: "NOT_FOUND_CLASS" };
+            }
+            const result = await Class.findById(foundClass)
+                .select("student")
+                .populate({ path: "student", select: "fullName score" });
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async classFilterByYear(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    const { yearAcademic } = req.body;
-    try {
-      const foundYearAcademic = await Class.findOne({
-        yearAcademic: yearAcademic,
-      });
-      if (!foundYearAcademic) {
-        throw { name: "NOT_FOUND_YEAR" };
-      }
-      res.status(200).json(foundYearAcademic);
-    } catch (error) {
-      next(error);
+    static async classFilterByYear(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        const { yearAcademic } = req.body;
+        try {
+            const foundYearAcademic = await Class.findOne({
+                yearAcademic: yearAcademic,
+            });
+            if (!foundYearAcademic) {
+                throw { name: "NOT_FOUND_YEAR" };
+            }
+            res.status(200).json(foundYearAcademic);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async findAllClass(req: Request, res: Response, next: NextFunction) {
-    try {
-      const result = await Class.find()
-        .populate({ path: "homeTeacher", select: "fullName" })
-        .populate({ path: "student", select: "fullName" })
-        .populate({
-          path: "schedule",
-          select: "hari course startTeach endTeach",
-        });
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
+    static async findAllClass(req: Request, res: Response, next: NextFunction) {
+        try {
+            const result = await Class.find()
+                .populate({ path: "homeTeacher", select: "fullName" })
+                .populate({ path: "student", select: "fullName" })
+                .populate({
+                    path: "schedule",
+                    select: "hari course startTeach endTeach",
+                });
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async findClass(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
-    try {
-      const foundClass = await Class.findById(id);
-      if (!foundClass) {
-        throw { name: "NOT_FOUND_CLASS" };
-      }
-      const result = await Class.findById(foundClass);
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
+    static async findClass(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        try {
+            const foundClass = await Class.findById(id);
+            if (!foundClass) {
+                throw { name: "NOT_FOUND_CLASS" };
+            }
+            const result = await Class.findById(foundClass);
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async updateClass(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
-    const { className, yearAcademic, homeTeacher, schedule } = req.body;
-    try {
-      const foundClass = await Class.findById(id);
-      if (!foundClass) {
-        throw { name: "NOT_FOUND_CLASS" };
-      }
-      const foundhomeTeacher = await Teacher.findById({
-        _id: homeTeacher,
-      });
-      if (!foundhomeTeacher) {
-        throw { name: "NOT_FOUND_HOMEROOM" };
-      }
-      const foundSchedule = await Schedule.findById({ _id: schedule });
-      if (!foundSchedule) {
-        throw { name: "NOT_FOUND_SCHEDULE" };
-      }
-      const result = await Class.findByIdAndUpdate(
-        foundClass,
-        {
-          className: className,
-          yearAcademic: yearAcademic,
-          homeTeacher: foundhomeTeacher,
-          schedule: foundSchedule,
-        },
-        { new: true }
-      );
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
+    static async updateClass(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        const { className, yearAcademic, homeTeacher, schedule } = req.body;
+        try {
+            const foundClass = await Class.findById(id);
+            if (!foundClass) {
+                throw { name: "NOT_FOUND_CLASS" };
+            }
+            const foundhomeTeacher = await Teacher.findById({
+                _id: homeTeacher,
+            });
+            if (!foundhomeTeacher) {
+                throw { name: "NOT_FOUND_HOMEROOM" };
+            }
+            const foundSchedule = await Schedule.findById({ _id: schedule });
+            if (!foundSchedule) {
+                throw { name: "NOT_FOUND_SCHEDULE" };
+            }
+            const result = await Class.findByIdAndUpdate(
+                foundClass,
+                {
+                    className: className,
+                    yearAcademic: yearAcademic,
+                    homeTeacher: foundhomeTeacher,
+                    schedule: foundSchedule,
+                },
+                { new: true }
+            );
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async deleteClass(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.params;
-    try {
-      const foundClass = await Class.findById({ _id: id });
-      if (!foundClass) {
-        throw { name: "NOT_FOUND_CLASS" };
-      }
-      const result = await Class.findByIdAndDelete(foundClass);
-      res.status(200).json(result);
-    } catch (error) {}
-  }
-
-  static async changeClass(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    const { student, className } = req.body;
-    console.log(student)
-    console.log(className)
-    try {
-      const foundStudent = await Student.findById(student);
-      console.log(foundStudent)
-      if (!foundStudent) {
-        throw { name: "NOT_FOUND_STUDENT" };
-      }
-      
-      const foundClass = await Class.findById(className);
-      console.log(foundClass)
-      if (!foundClass) {
-        throw { name: "NOT_FOUND_CLASS" };
-      }
-    
-      const result = await Student.findByIdAndUpdate(
-        student,
-        {
-          classes: foundClass,
-        },
-        { new: true }
-      );
-      res.status(200).json(result);
-    } catch (error) {
-      console.log((error as Error).name);
-      next(error);
+    static async deleteClass(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        try {
+            const foundClass = await Class.findById({ _id: id });
+            if (!foundClass) {
+                throw { name: "NOT_FOUND_CLASS" };
+            }
+            const result = await Class.findByIdAndDelete(foundClass);
+            res.status(200).json(result);
+        } catch (error) {}
     }
-  }
+
+    static async changeClass(req: Request, res: Response, next: NextFunction) {
+        const { student } = req.params;
+        const { className } = req.body;
+        console.log(student);
+        console.log(className);
+        try {
+            const foundStudent = await Student.findById(student);
+            console.log(foundStudent);
+            if (!foundStudent) {
+                throw { name: "NOT_FOUND_STUDENT" };
+            }
+
+            const foundClass = await Class.findById(className);
+            console.log(foundClass);
+            if (!foundClass) {
+                throw { name: "NOT_FOUND_CLASS" };
+            }
+
+            const result = await Student.findByIdAndUpdate(
+                student,
+                {
+                    classes: foundClass,
+                },
+                { new: true }
+            );
+            res.status(200).json(result);
+        } catch (error) {
+            console.log((error as Error).name);
+            next(error);
+        }
+    }
 }
 
 export default ClassConttroller;
--- routes/headmaster.routes.ts
+++ routes/headmaster.routes.ts
@@ -8,81 +8,105 @@ import TeacherController from "../controllers/teacher.controller";
 import authJwt from "../middlewares/authJwt";
 
 class headmasterRoutes {
-  public headmasterRoute: Router;
-  constructor() {
-    this.headmasterRoute = Router();
-    this.headmasterControlTeacher();
-    this.headmasterControlStudent();
-    this.headmasterControlParent();
-    this.headmasterControlClasses();
-    this.headmasterControlCourse();
-    this.headmasterControlSchedule();
-  }
-  protected headmasterControlTeacher = () => {
-    this.headmasterRoute.post(
-      "/teacher/create",
-      TeacherController.createTeacher
-    );
-    this.headmasterRoute.get("/teacher", TeacherController.findAllTeacher);
-    this.headmasterRoute.get("/teacher/:id", TeacherController.findTeacher);
-    this.headmasterRoute.put("/teacher/:id", TeacherController.updateTeacher);
-    this.headmasterRoute.post(
-      "/teacher/add-course/:id",
-      TeacherController.addSpesificCourse
-    );
-    this.headmasterRoute.put(
-      "/teacher/update-course/:id",
-      TeacherController.updateSpesificCourse
-    );
-  };
-  protected headmasterControlStudent = () => {
-    this.headmasterRoute.post(
-      "/student/create",
-      StudentController.createStudent
-    );
-    this.headmasterRoute.get("/student", StudentController.findAllStudent);
-    this.headmasterRoute.get("/student/:id", StudentController.findStudent);
-    this.headmasterRoute.put("/student/:id", StudentController.updateStudent);
-  };
-  protected headmasterControlParent = () => {
-    this.headmasterRoute.post("/parent/create", ParentController.createParent);
-    this.headmasterRoute.get("/parent", ParentController.findAllParent);
-    this.headmasterRoute.get("/parent/:id", ParentController.findParent);
-    this.headmasterRoute.put("/parent/:id", ParentController.updateParent);
-  };
-  protected headmasterControlClasses = () => {
-    this.headmasterRoute.post("/class/create", ClassConttroller.createClass);
-    this.headmasterRoute.put(
-      "/class/set-hometeach",
-      ClassConttroller.setHomeroom
-    );
-    this.headmasterRoute.get("/class/score/:id", ClassConttroller.scorebyClass);
-    this.headmasterRoute.get("/class/year", ClassConttroller.classFilterByYear);
-    this.headmasterRoute.get("/class", ClassConttroller.findAllClass);
-    this.headmasterRoute.get("/class/:id", ClassConttroller.findClass);
-    this.headmasterRoute.put("/class/:id", ClassConttroller.updateClass);
-    this.headmasterRoute.put("/class/change", ClassConttroller.changeClass);
-  };
-  protected headmasterControlCourse = () => {
-    this.headmasterRoute.post("/course/create", coursesController.createCourse);
-    this.headmasterRoute.get("/course", coursesController.findAllCourse);
-    this.headmasterRoute.get("/course/:id", coursesController.findCourse);
-  };
-  protected headmasterControlSchedule = () => {
-    this.headmasterRoute.post(
-      "/schedule/create",
-      scheduleController.createSchedule
-    );
-    this.headmasterRoute.get("/schedule", scheduleController.listSchedule);
-    this.headmasterRoute.get(
-      "/schedule/:id",
-      scheduleController.spesificSchedule
-    );
-    this.headmasterRoute.put(
-      "/schedule/:id",
-      scheduleController.updateSchedule
-    );
-  };
+    public headmasterRoute: Router;
+    constructor() {
+        this.headmasterRoute = Router();
+        this.headmasterControlTeacher();
+        this.headmasterControlStudent();
+        this.headmasterControlParent();
+        this.headmasterControlClasses();
+        this.headmasterControlCourse();
+        this.headmasterControlSchedule();
+    }
+    protected headmasterControlTeacher = () => {
+        this.headmasterRoute.post(
+            "/teacher/create",
+            TeacherController.createTeacher
+        );
+        this.headmasterRoute.get("/teacher", TeacherController.findAllTeacher);
+        this.headmasterRoute.get("/teacher/:id", TeacherController.findTeacher);
+        this.headmasterRoute.put(
+            "/teacher/:id",
+            TeacherController.updateTeacher
+        );
+        this.headmasterRoute.post(
+            "/teacher/add-course/:id",
+            TeacherController.addSpesificCourse
+        );
+        this.headmasterRoute.put(
+            "/teacher/update-course/:id",
+            TeacherController.updateSpesificCourse
+        );
+    };
+    protected headmasterControlStudent = () => {
+        this.headmasterRoute.post(
+            "/student/create",
+            StudentController.createStudent
+        );
+        this.headmasterRoute.get("/student", StudentController.findAllStudent);
+        this.headmasterRoute.get("/student/:id", StudentController.findStudent);
+        this.headmasterRoute.put(
+            "/student/:id",
+            StudentController.updateStudent
+        );
+    };
+    protected headmasterControlParent = () => {
+        this.headmasterRoute.post(
+            "/parent/create",
+            ParentController.createParent
+        );
+        this.headmasterRoute.get("/parent", ParentController.findAllParent);
+        this.headmasterRoute.get("/parent/:id", ParentController.findParent);
+        this.headmasterRoute.put("/parent/:id", ParentController.updateParent);
+    };
+    protected headmasterControlClasses = () => {
+        this.headmasterRoute.post(
+            "/class/create",
+            ClassConttroller.createClass
+        );
+        this.headmasterRoute.put(
+            "/class/set-hometeach",
+            ClassConttroller.setHomeroom
+        );
+        this.headmasterRoute.get(
+            "/class/score/:id",
+            ClassConttroller.scorebyClass
+        );
+        this.headmasterRoute.get(
+            "/class/year",
+            ClassConttroller.classFilterByYear
+        );
+        this.headmasterRoute.get("/class", ClassConttroller.findAllClass);
+        this.headmasterRoute.get("/class/:id", ClassConttroller.findClass);
+        this.headmasterRoute.put("/class/:id", ClassConttroller.updateClass);
+        this.headmasterRoute.put(
+            "/class/change/:student",
+            ClassConttroller.changeClass
+        );
+    };
+    protected headmasterControlCourse = () => {
+        this.headmasterRoute.post(
+            "/course/create",
+            coursesController.createCourse
+        );
+        this.headmasterRoute.get("/course", coursesController.findAllCourse);
+        this.headmasterRoute.get("/course/:id", coursesController.findCourse);
+    };
+    protected headmasterControlSchedule = () => {
+        this.headmasterRoute.post(
+            "/schedule/create",
+            scheduleController.createSchedule
+        );
+        this.headmasterRoute.get("/schedule", scheduleController.listSchedule);
+        this.headmasterRoute.get(
+            "/schedule/:id",
+            scheduleController.spesificSchedule
+        );
+        this.headmasterRoute.put(
+            "/schedule/:id",
+            scheduleController.updateSchedule
+        );
+    };
 }
 
 const headmasterRouter = new headmasterRoutes().headmasterRoute;
